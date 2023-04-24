44da317d1ba7da9a03e0e6b36697188885e14766
AnikenC <quantac47@gmail.com>
2023-04-24 17:56:36 +0800

    Updated Tuner

--- QuantumControl/algo_tuner.py
+++ QuantumControl/algo_tuner.py
@@ -4,8 +4,10 @@ from cleanrl_utils.tuner import Tuner
 
 import quantum_envs
 
+### Try to always use a "direct_algo.py" as the script to avoid excessive registration times ###
+
 tuner = Tuner(
-    script="ppo.py",
+    script="direct_ppo.py",
     metric="charts/episodic_return",
     metric_last_n_average_window=50,
     direction="maximize",
@@ -14,14 +16,14 @@ tuner = Tuner(
         "quantum_envs/QuantumGateCalibration-v0": [0, 5],
     },
     params_fn=lambda trial: {
-        "learning-rate": trial.suggest_loguniform("learning-rate", 0.0003, 0.003),
+        #"learning-rate": trial.suggest_loguniform("learning-rate", 0.0003, 0.003),
         "num-minibatches": trial.suggest_categorical("num-minibatches", [1, 2, 4]),
         "update-epochs": trial.suggest_categorical("update-epochs", [1, 2, 4, 8]),
         "num-steps": trial.suggest_categorical("num-steps", [5, 16, 32, 64, 128]),
-        "vf-coef": trial.suggest_uniform("vf-coef", 0, 5),
-        "max-grad-norm": trial.suggest_uniform("max-grad-norm", 0, 5),
-        "total-timesteps": 100000,
-        "num-envs": 2,
+        #"vf-coef": trial.suggest_uniform("vf-coef", 0, 5),
+        #"max-grad-norm": trial.suggest_uniform("max-grad-norm", 0, 5),
+        #"total-timesteps": 100000,
+        #"num-envs": 2,
     },
     pruner=optuna.pruners.MedianPruner(n_startup_trials=5),
     sampler=optuna.samplers.TPESampler(),
