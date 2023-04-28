6b74f5dcd65545d0a5dcd8254f232715402c0e45
AnikenC <quantac47@gmail.com>
2023-04-28 12:18:10 +0800

    Updated

--- QuantumControl/ppo.py
+++ QuantumControl/ppo.py
@@ -71,7 +71,7 @@ def parse_args():
         help="the surrogate clipping coefficient")
     parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
-    parser.add_argument("--ent-coef", type=float, default=0.0,
+    parser.add_argument("--ent-coef", type=float, default=0.001,
         help="coefficient of the entropy")
     parser.add_argument("--vf-coef", type=float, default=0.5,
         help="coefficient of the value function")
